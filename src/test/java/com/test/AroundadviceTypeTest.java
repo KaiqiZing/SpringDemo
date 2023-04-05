36ac43a44af7d7890ec629a4b825a24ffb6fa9e3
kaiqi <zhang20170730@gmail.com>
2023-04-05 17:19:36 +0800

    Spring 环绕通知配置

--- src/test/java/com/test/AroundadviceTypeTest.java
+++ src/test/java/com/test/AroundadviceTypeTest.java
@@ -4,6 +4,8 @@ import com.springadviceType.service.IAccountService;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+/*Spring 环绕通知执行方法*/
 public class AroundadviceTypeTest {
     public static void main(String[] args) {
         ApplicationContext ac = new ClassPathXmlApplicationContext("AroundadviceType.xml");
